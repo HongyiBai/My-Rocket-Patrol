d14a41ef66a8f780808337cd89212e007667d5b2
Hongyi Bai <hobai@ucsc.edu>
2022-04-10 22:25:21 -0700

    animations and restar function

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -24,6 +24,46 @@ keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
 keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
 keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+this.anims.create({
+    key: 'explode',
+    frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+    frameRate: 30
+});
+
+
+  this.p1Score = 0;
+  let scoreConfig = {
+    fontFamily: 'Courier',
+    fontSize: '28px',
+    backgroundColor: '#F3B141',
+    color: '#843605',
+    align: 'right',
+    padding: {
+      top: 5,
+      bottom: 5,
+    },
+    fixedWidth: 100
+  }
+  this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+    scoreConfig.fixedWidth = 0;
+    this.clock = this.time.delayedCall(60000, () => {
+    this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+    }, null, this);
+
+    this.gameOver = false;
+
+
+    scoreConfig.fixedWidth = 0;
+    this.clock = this.time.delayedCall(60000, () => {
+    this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+    this.gameOver = true;
+}, null, this);
+
+
       }
       preload() {
         
@@ -33,25 +73,31 @@ keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
       }
       update() {
-        this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
-        this.ship01.update();              
-        this.ship02.update();
-        this.ship03.update();
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }  
+    
+        if (!this.gameOver) {               
+            this.p1Rocket.update();         
+            this.ship01.update();           
+            this.ship02.update();
+            this.ship03.update();
+        } 
 
+      
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
-            this.p1Rocket.reset();
-            this.ship03.reset();
-          }
-          if (this.checkCollision(this.p1Rocket, this.ship02)) {
-            this.p1Rocket.reset();
-            this.ship02.reset();
-          }
-          if (this.checkCollision(this.p1Rocket, this.ship01)) {
-            this.p1Rocket.reset();
-            this.ship01.reset();
-          }
-      }
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship03);   
+            }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship02);
+            }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+        this.p1Rocket.reset();
+        this.shipExplode(this.ship01);
+            }
+        }
       checkCollision(rocket, ship) {
         
         if (rocket.x < ship.x + ship.width && 
@@ -63,4 +109,18 @@ keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
             return false;
         }
     }
+    shipExplode(ship) {
+        
+        ship.alpha = 0;
+        
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             
+        boom.on('animationcomplete', () => {    
+          ship.reset();                         
+          ship.alpha = 1;                       
+          boom.destroy();                       
+        });  
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;      
+      }
 }
