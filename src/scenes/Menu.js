9b86b9ce2017aaee77cab0a4a0e157a121619b43
Hongyi Bai <hobai@ucsc.edu>
2022-04-11 00:14:08 -0700

    debug for restart game and timer

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -37,7 +37,7 @@ class Menu extends Phaser.Scene {
           
           game.settings = {
             spaceshipSpeed: 3,
-            gameTimer: 60000    
+            gameTimer: 60000   
           }
           this.sound.play('sfx_select');
           this.scene.start('playScene');    
