66648af399f9f306d5e960c4efb202a5d6e37f64
Hongyi Bai <hobai@ucsc.edu>
2022-04-11 00:18:48 -0700

    check for web games

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -45,7 +45,7 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
           
           game.settings = {
-            spaceshipSpeed: 5,
+            spaceshipSpeed: 6,
             gameTimer: 45000    
           }
           this.sound.play('sfx_select');
